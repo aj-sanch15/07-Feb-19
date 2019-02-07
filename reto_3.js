93a429da4fa617a16a534490c4b76fb9b62acdb2
aj-sanch15 <42626620+aj-sanch15@users.noreply.github.com>
2019-02-07 09:18:57 -0600

    Create reto_3.js

---
+++ reto_3.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(255);
+	strokeWeight(20);
+	stroke(255,0,0);
+	ellipse(200,200,200,200);
+	strokeWeight(20);
+	stroke(255,0,0);
+	line(400/3,400/3,(400/3)*2,(400/3)*2);
+	
+	
+	
+}
