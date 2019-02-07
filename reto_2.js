6419d6fd4cef603684498a8ff89d575799e25274
aj-sanch15 <42626620+aj-sanch15@users.noreply.github.com>
2019-02-07 09:14:16 -0600

    Create reto_2.js

---
+++ reto_2.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(0);
+	fill(0,255,0);
+	rect(400/3,0,400/3,400);
+	fill(255,0,0);
+	rect(0,0,400/3,400);
+	fill(0,0,255);
+	rect((400/3)*2,0,400/3,400);
+	
+	
+	
+}
