de55e2608b9602f579404011213c9890917e5b2f
aj-sanch15 <42626620+aj-sanch15@users.noreply.github.com>
2019-02-07 09:13:55 -0600

    Create reto_1.js

---
+++ reto_1.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(0);
+	line(200,0,200,200);
+	stroke(255);
+	line(0,400,200,200);
+	stroke(255);
+	line(400,400,200,200);
+	stroke(255);
+	
+	
+}
